9cb42b0b99129a891fc87655e819d0b4839866ec
manishnewprojects <manishnewprojects@gmail.com>
2017-11-20 19:58:18 -0800

    update config settings

--- php/data.php
+++ php/data.php
@@ -6,8 +6,7 @@ include dirname(__FILE__)."/../php_includes/addr_lookup.php";
 // Database details
 $db_server   = 'localhost';
 $db_username = 'root';
-//$db_password = '*****qackJBRK';
-$db_password = '****pper';
+$db_password = '*********';
 $db_name     = 'w411_listings_database';
 
 // Get job (and id)
@@ -68,7 +67,7 @@ if ($job != ''){
         $wa_str = '';
         $wa_str = '<a href="https://api.whatsapp.com/send?phone=';
         $wa_str .= $company['whatsapp'];
-        $wa_str .= '&text=New customer query via whats411.com! Response requested"> Click to message</a>';
+        $wa_str .= '&text=New customer query via whats411.com! Response requested"> Send WhatsApp message</a>';
         $mysql_data[] = array(
           "name"            => $company['name'],
           "owner"           => $company['owner'],
